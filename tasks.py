00e6498a3ef6f56e2fc0f146c3be4443229ff571
Tomer <tsherf10@gmail.com>
2022-04-02 12:02:21 +0300

    Added stuff

--- tasks.py
+++ tasks.py
@@ -10,7 +10,7 @@ from metrics import accuracy, f1_score
 def get_task_items(task_name: str, datasets_path: str, vocab_path: str, max_seq_len: int) -> tuple:
     assert task_name is not None, "Enter task_name."
     task = task_name.lower()
-    assert task in ["mrpc", "mnli", "cola"], f"Invalid task_name: {task}"
+    assert task in ["mrpc", "mnli", "cola", "rte"], f"Invalid task_name: {task}"
 
     if task == "mrpc":
         return mrpc_task(datasets_path, vocab_path, max_seq_len)
@@ -20,6 +20,9 @@ def get_task_items(task_name: str, datasets_path: str, vocab_path: str, max_seq_
 
     elif task == "cola":
         return cola_task(datasets_path, vocab_path, max_seq_len)
+
+    elif task == "rte":
+        return rte_task(datasets_path, vocab_path, max_seq_len)
 
 
 def mrpc_task(datasets_path: str, vocab_path: str, max_seq_len: int) -> tuple:
@@ -83,6 +86,34 @@ def cola_task(datasets_path: str, vocab_path: str, max_seq_len: int) -> tuple:
             text = tokenization.convert_to_unicode(row[3])
             data.append([label, text])
         input_ids, token_type_ids, labels = prep_single_sentence_data(data, vocab_path, max_seq_len)
+        datasets.append(GlueDataset(input_ids, token_type_ids, labels))
+
+    train_dataset, dev_dataset = datasets
+    eval_metrics = [('accuracy', accuracy)]
+    num_classes = 2
+
+    return num_classes, train_dataset, dev_dataset, eval_metrics
+
+
+def rte_task(datasets_path: str, vocab_path: str, max_seq_len: int) -> tuple:
+    data_path = os.path.join(datasets_path, "glue_data/RTE/")
+    raw_train = read_tsv_file(os.path.join(data_path, "train.tsv"))
+    raw_dev = read_tsv_file(os.path.join(data_path, "dev.tsv"))
+
+    label_str_to_int_dict = {
+        "not_entailment": 0,
+        "entailment": 1
+    }
+
+    datasets = []
+    for raw_data in [raw_train, raw_dev]:
+        data = []
+        for row in raw_data[1:]:
+            label = label_str_to_int_dict[tokenization.convert_to_unicode(row[3])]
+            text_a = tokenization.convert_to_unicode(row[1])
+            text_b = tokenization.convert_to_unicode(row[2])
+            data.append([label, text_a, text_b])
+        input_ids, token_type_ids, labels = prep_sentence_pairs_data(data, vocab_path, max_seq_len)
         datasets.append(GlueDataset(input_ids, token_type_ids, labels))
 
     train_dataset, dev_dataset = datasets
--- utils.py
+++ utils.py
@@ -118,23 +118,3 @@ def set_random_seed(seed: int) -> None:
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-
-
-# def check_accuracy(loader, model, device):
-#     num_correct = 0
-#     num_samples = 0
-#     model.eval()  # set model to evaluation mode
-#     with torch.no_grad():
-#         for x, y in loader:
-#             x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
-#             y = y.to(device=device, dtype=torch.long)
-#             scores = model(x)
-#             _, preds = scores.max(1)
-#             num_correct += (preds == y).sum()
-#             num_samples += preds.size(0)
-#         acc = float(num_correct) / num_samples
-#         print('Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * acc))
-
-
-
-

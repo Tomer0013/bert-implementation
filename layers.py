38302f653e0434c27400a6b2ba9a1128534c7453
Tomer <tsherf10@gmail.com>
2022-04-13 10:11:54 +0300

    Checkpoint

--- layers.py
+++ layers.py
@@ -3,61 +3,6 @@ import torch.nn as nn
 import math
 
 
-# def masked_softmax(logits: torch.Tensor, mask: torch.Tensor, dim: int = -1, log_softmax: bool = False) -> torch.Tensor:
-#     """Take the softmax of `logits` over given dimension, and set
-#     entries to 0 wherever `mask` is 0.
-#
-#     Args:
-#         logits (torch.Tensor): Inputs to the softmax function.
-#         mask (torch.Tensor): Same shape as `logits`, with 0 indicating
-#             positions that should be assigned 0 probability in the output.
-#         dim (int): Dimension over which to take softmax.
-#         log_softmax (bool): Take log-softmax rather than regular softmax.
-#             E.g., some PyTorch functions such as `F.nll_loss` expect log-softmax.
-#     """
-#     mask = mask.type(torch.float32)
-#     masked_logits = mask * logits + (1 - mask) * -1e30
-#     softmax_fn = nn.functional.log_softmax if log_softmax else nn.functional.softmax
-#     probs = softmax_fn(masked_logits, dim)
-#
-#     return probs
-#
-#
-# class MultiHeadAttention(nn.Module):
-#     """
-#     Multi headed attention as described in the paper
-#     "Attention is All You Need".
-#
-#     Args:
-#         input_size (int): Dim size of input.
-#         num_heads (int): Number of heads.
-#         attn_drop_prob (float): Dropout probability right after the softmax.
-#     """
-#     def __init__(self, input_size: int, num_heads: int, attn_drop_prob: float = 0.0) -> None:
-#         assert input_size % num_heads == 0, "Input size divided by num heads must be a whole number."
-#         super(MultiHeadAttention, self).__init__()
-#         self.num_heads = num_heads
-#         self.attn_drop_prob = attn_drop_prob
-#         self.q_proj = torch.nn.Linear(input_size, input_size)
-#         self.k_proj = torch.nn.Linear(input_size, input_size)
-#         self.v_proj = torch.nn.Linear(input_size, input_size)
-#         self.o_proj = torch.nn.Linear(input_size, input_size)
-#
-#     def forward(self, x: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
-#         n, t, d = x.shape
-#
-#         q = self.q_proj(x).view(n, t, self.num_heads, d // self.num_heads).transpose(1, 2)
-#         k = self.k_proj(x).view(n, t, self.num_heads, d // self.num_heads).transpose(1, 2)
-#         v = self.v_proj(x).view(n, t, self.num_heads, d // self.num_heads).transpose(1, 2)
-#
-#         s = q.matmul(k.transpose(-2, -1)) / math.sqrt(d // self.num_heads)
-#         s = masked_softmax(s, mask.view(n, 1, 1, -1))
-#         s = torch.dropout(s, self.attn_drop_prob, self.training)
-#         a = s.matmul(v).transpose(1, 2).contiguous().view(n, t, -1)
-#         x = self.o_proj(a)
-#
-#         return x
-
 class MultiHeadAttention(nn.Module):
     """
     Multi headed attention as described in the paper
